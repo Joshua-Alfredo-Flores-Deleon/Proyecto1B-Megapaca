b4a5640229b6567dad013c5063ea4878c4c1e82a
Joshua-Alfredo-Flores-Deleon <20240228@ricaldone.edu.sv>
2026-05-25 11:56:06 -0600

    Se agrego el carrito con arrays

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ import cors from "cors";
 import recoveryPasswordRoutes from "./src/routes/recoveryPassword.js";
 import providerRoutes from "./src/routes/provider.js";
 import limiter from "./src/middlewares/limiter.js"
+import cartRoutes from "./src/routes/cart.js"
 
 //Creo una constante que guarde Express
 const app = express();
@@ -39,5 +40,6 @@ app.use("/api/login", loginCustomerRoutes);
 app.use("/api/logout", logoutRoutes);
 app.use("/api/recoveryPassword", recoveryPasswordRoutes);
 app.use("/api/providers", providerRoutes);
+app.use("/api/cart", cartRoutes);
 
 export default app;
--- backend/src/middlewares/limiter.js
+++ backend/src/middlewares/limiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit"
 
 const limiter = rateLimit({
-    windows: 5 * 60 *1000, //5 minutos
-    max: 5, //Maximo de solicitudes HTTP
+    windowMs: 5 * 60 *1000, //5 minutos
+    max: 100, //Maximo de solicitudes HTTP
     message: {
         status: 429,
         error: "Too many Request"
